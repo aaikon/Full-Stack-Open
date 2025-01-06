845fb0a9242905a751e09f91670bcf712d7c0b0c
Arttu <home@arttu-2.local>
2025-01-06 15:41:10 +0200

    part2 finished + changes to test

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,33 +1,73 @@
 import { useState, useEffect } from 'react'
 import exampleService from './services/example'
-import axios from 'axios'
+import axios, { all } from 'axios'
+
+const Countries = ({ countries, setValue }) => {
+  const l = countries.length
+
+  if (l > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    )
+  } else if (l > 1) {
+    return (
+      <div>
+        {countries.map(country => (
+          <p key={country.name}>{country.name} <button onClick={() => setValue(country.name)}>show</button></p>
+        ))}
+      </div>
+    )
+  } else if (l === 1) {
+    const country = countries[0]
+    return (
+      <div>
+        <h1>{country.name}</h1>
+        <div>
+          <p>capital {country.capital}</p>
+          <p>area {country.area}</p>
+        </div>
+        <h3>languages:</h3>
+        <p>{JSON.stringify(country.languages)}</p>
+        <img src={country.flag} alt="image of a flag" />
+      </div>
+    )
+  }
+}
 
 const App = () => {
   const [value, setValue] = useState('')
-  const [countries, setCountries] = useState(null)
+  const [countries, setCountries] = useState([])
+  const [allCountries, setAllCountries] = useState(null)
 
   useEffect(() => {
- 
-    if (value && value.length > 0) {
+    if (!allCountries) {
       axios
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${RegExp(`\\b\\w*${value}\\w*\\b`, 'i')}`)
-        .then(response => {
-          setCountries(response.data.name)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-      }
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then(response => {
+        setAllCountries(response.data.map(country => ({
+          name: country.name.common,
+          capital: country.capital,
+          area: country.area,
+          languages: country.languages,
+          flag: country.flags.png,
+          weather: {
+            temp: 1
+          }
+        })))
+      })
+    } else {
+      setCountries(allCountries.filter(country => country.name.toLowerCase().includes(value.toLowerCase())))
+    }
   }, [value])
 
-  const handleChange = (event) => setValue(event.target.value)
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
 
   return (
     <div>
       find countries <input value={value} onChange={handleChange}/>
-      <pre>
-        {JSON.stringify(countries, null, 2)}
-      </pre>
+      <Countries countries={countries} setValue={setValue}/>
     </div>
   )
 }
